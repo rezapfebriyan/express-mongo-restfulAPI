2797505be244c59593ec4940d75356689544de79
rezapfebriyan <rezafebriyan00@gmail.com>
2023-01-07 20:54:34 +0700

    add endpoint update

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -15,13 +15,31 @@ export const getProducts = async (req, res) => {
     }
 }
 
-export const storeProducts = async (req, res) => {
+export const storeProduct = async (req, res) => {
     const product = new Product(req.body)
     try {
         const save_product = await product.save()
         res
             .status(201)
             .json(save_product)
+    } catch (error) {
+        res
+            .status(400)
+            .json({
+            message: error.message
+        })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    try {
+        const update_product = await Product.updateOne(
+            {_id: req.params.id},
+            {$set: req.body}
+            )
+        res
+            .status(200)
+            .json(update_product)
     } catch (error) {
         res
             .status(400)
--- Route/index.js
+++ Route/index.js
@@ -1,9 +1,19 @@
 import express from "express"
-import { getProducts, storeProducts } from "../Controllers/ProductController.js"
+import { 
+    getProducts,
+    storeProduct,
+    updateProduct
+ } from "../Controllers/ProductController.js"
 
 const router = express.Router()
 
+//! === L I S T ===
 router.get('/', getProducts)
-router.post('/', storeProducts)
+
+//! === S T O R E ===
+router.post('/', storeProduct)
+
+//! === U P D A T E ===
+router.patch('/:id', updateProduct)
 
 export default router
